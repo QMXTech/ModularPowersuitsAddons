9b7530ef97e9450fb75fce425bf424da9e053e58
Korynkai <matt@qmxtech.com>
2015-02-18 14:30:31 +0000

    Fix missing import

--- src/main/java/com/qmxtech/powersuitaddons/common/ModularPowersuitsAddons.java
+++ src/main/java/com/qmxtech/powersuitaddons/common/ModularPowersuitsAddons.java
@@ -12,6 +12,7 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.event.FMLServerStartedEvent;
 import cpw.mods.fml.common.network.FMLEventChannel;
 import cpw.mods.fml.common.network.NetworkRegistry;
